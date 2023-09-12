856db3fd93d523b83ff58d24a8e57519b3d9abf8
Nkwi Cyril Akinimbom <akinimbomnkwi@gmail.com>
2023-09-12 22:56:58 +0100

    kickstart

---
+++ basics/Main.java
@@ -0,0 +1,12 @@
+package basics;
+
+/**
+ * main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("This is an interesting moment with java");
+    }
+}
