8675eac67b012268cab7f649f5b219375dad389a
Nkwi Cyril Akinimbom <akinimbomnkwi@gmail.com>
2023-09-15 09:07:39 +0100

    working with variables

---
+++ basics/Variables.java
@@ -0,0 +1,20 @@
+package basics;
+
+public class Variables {
+    public static void main(String[] args) {
+        // concatenation of variables using the + character
+
+        int age = 21; // single variable declaration
+        int x = 2, y =3, z = 4; // multiple variable declaration
+
+        String name = "Nkwi Cyril";
+        System.out.println(name + " is " + age + " years old.");
+        System.out.println(x+y+z);
+        String firstName = "Nkwi ";
+        String lastName = "Cyril";
+        String finalName = firstName + lastName;
+
+        System.out.println(finalName);
+
+    }
+}
