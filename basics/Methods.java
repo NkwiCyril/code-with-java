660eb61de94eb6b92ec81d69ce5741c0b371f1a9
Nkwi Cyril Akinimbom <akinimbomnkwi@gmail.com>
2023-09-19 02:00:52 +0100

    done a few modifications

--- basics/Methods.java
+++ basics/Methods.java
@@ -1,6 +1,7 @@
 package basics;
 
 public class Methods {
+
     static void myMethod(int i) {
         System.out.println("This is a static method.");
         System.out.println("It cannot be accessed ny objects of this class.");
@@ -9,9 +10,18 @@ public class Methods {
         System.out.println("This is the random phone you have choosen: " + value[i]);
     }
 
+    static String checkAge(int age) {
+        String checker = (age >= 18) 
+                        ? "Access Granted! You are the required age" 
+                        : "Access Denied! You are not the required age";
+        return checker;
+    }
+
     public static void main(String[] args) {
         myMethod(1);
         System.out.println();
+        System.out.println(checkAge(0));
+        
     }
 
 }
